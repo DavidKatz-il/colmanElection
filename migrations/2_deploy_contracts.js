c20863b83b3d6ec418a1beffcd5a7597b32166bd
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2021-02-12 14:52:59 +0200

    str -> int

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Election = artifacts.require("./Election.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Election, "ColmanCoin", "BSC", "1000000");
+  deployer.deploy(Election, "ColmanCoin", "BSC", 100000);
 };
